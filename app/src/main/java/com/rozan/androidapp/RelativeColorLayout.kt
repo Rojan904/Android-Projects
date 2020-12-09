ee6914288acb4a205daf18fe27e82594d740ab69
Rojan904 <57635028+Rojan904@users.noreply.github.com>
2020-12-09 10:57:18 +0545

    Relative Layout designs added.

---
+++ app/src/main/java/com/rozan/androidapp/RelativeColorLayout.kt
@@ -0,0 +1,13 @@
+package com.rozan.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeColorLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_color_layout)
+
+    }
+}
